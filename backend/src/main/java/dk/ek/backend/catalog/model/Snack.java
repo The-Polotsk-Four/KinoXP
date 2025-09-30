84def1b2e8bc613bfe2b0f9a8828bde06c180043
MuniGlitch <chni0006@stud.kea.dk>
2025-09-30 13:58:57 +0200

    README.md

---
+++ backend/src/main/java/dk/ek/backend/catalog/model/Snack.java
@@ -0,0 +1,61 @@
+package dk.ek.backend.catalog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Snack {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(int dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private int dateOfPurchase;
+    private int quantity;
+
+
+}
