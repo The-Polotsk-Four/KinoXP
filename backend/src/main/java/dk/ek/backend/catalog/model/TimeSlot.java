bec5a47ac769b4f679e509d81d5348d2d8e2dfcf
Harning <toha0009@stud.ek.dk>
2025-10-01 12:42:55 +0200

    made TimeSlot

---
+++ backend/src/main/java/dk/ek/backend/catalog/model/TimeSlot.java
@@ -0,0 +1,69 @@
+package dk.ek.backend.catalog.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class TimeSlot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private EmployeeRole role;
+    private User employee;
+
+    public TimeSlot() {
+    }
+
+    public TimeSlot(Long id, LocalDateTime startTime, LocalDateTime endTime, EmployeeRole role, User employee) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.role = role;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public EmployeeRole getRole() {
+        return role;
+    }
+
+    public void setRole(EmployeeRole role) {
+        this.role = role;
+    }
+
+    public User getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(User employee) {
+        this.employee = employee;
+    }
+}
