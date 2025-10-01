739e1c3b1720008ddf9e7698032706fbd1977485
Gustav <guwi0001@stud.kea.dk>
2025-10-01 11:47:45 +0200

    Lavet User controller, service, mapper og repository og fået en metode til at fungere

--- backend/src/main/java/dk/ek/backend/catalog/controller/UserController.java
+++ backend/src/main/java/dk/ek/backend/catalog/controller/UserController.java
@@ -1,4 +1,27 @@
 package dk.ek.backend.catalog.controller;
 
+import dk.ek.backend.catalog.dto.UserDto;
+import dk.ek.backend.catalog.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
 }
--- backend/src/main/java/dk/ek/backend/catalog/model/User.java
+++ backend/src/main/java/dk/ek/backend/catalog/model/User.java
@@ -3,8 +3,10 @@ package dk.ek.backend.catalog.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table (name = "users")
 public class User {
 
     @Id
