0a83384ae48e67515f7f9b554a5a118ed7a91de0
SofusVingaard <sovi0003@stud.kea.dk>
2025-10-07 09:28:02 +0200

    hallcontroller works. able to find all halls and by id

--- backend/src/main/java/dk/ek/backend/catalog/controller/HallController.java
+++ backend/src/main/java/dk/ek/backend/catalog/controller/HallController.java
@@ -4,6 +4,7 @@ import dk.ek.backend.catalog.dto.HallDto;
 import dk.ek.backend.catalog.service.HallService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,11 @@ public class HallController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getHallById(Long id){
-        return ResponseEntity.ok(hallService.getHallById(id));
-    }
+    public ResponseEntity getHallById(@PathVariable Long id){
+        try {
+            return ResponseEntity.ok(hallService.getHallById(id));
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }    }
 }
